ef21df1967a9e3c834f42de2254ef04004241ab7
yosefinaagst <yosefnaagustine@gmail.com>
2026-09-24 02:13:21 +0700

    code bahan ajaran

--- app/src/main/java/com/example/papbmodul/MainActivity.kt
+++ app/src/main/java/com/example/papbmodul/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.papbmodul.ui.theme.PAPBModulTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,94 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PAPBModulTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            DemoKotlin()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DemoKotlin() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Belajar Dasar Kotlin",
+        modifier = Modifier.padding(32.dp)
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PAPBModulTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun DemoKotlin() {
+//    Text(
+//        text = "Aplikasi Nilai Mahasiswa",
+//        modifier = Modifier.padding(32.dp)
+//    )
+//}
+
+//@Composable
+//fun DemoKotlin() {
+//    val nama = "Titin"
+//    val nilai = 85
+//
+//    Text(
+//        text = "Nama: $nama\nNilai: $nilai",
+//        modifier = Modifier.padding(32.dp)
+//    )
+//}
+
+//var dan val
+//val nilai = 85
+//nilai = 90
+//var nilai = 85
+//nilai = 90
+
+//null safety
+//@Composable
+//fun DemoKotlin() {
+//    val nama: String? = null
+//    val nilai = 85
+//
+//    val namaTampilan = nama ?: "Guest"
+//
+//    Text(
+//        text = "Nama: $namaTampilan\nNilai: $nilai",
+//        modifier = Modifier.padding(32.dp)
+//    )
+//}
+
+//bentuk kode keseluruhan
+//fun hitungNilaiAkhir(nilaiAwal: Int, bonus: Int = 0): Int {
+//    return nilaiAwal + bonus
+//}
+//
+//fun tentukanStatus(nilai: Int): String {
+//    return if (nilai >= 60) "Lulus" else "Tidak Lulus"
+//}
+//
+//fun tentukanGrade(nilai: Int): String {
+//    return when {
+//        nilai >= 80 -> "A"
+//        nilai >= 70 -> "B"
+//        nilai >= 60 -> "C"
+//        else -> "D"
+//    }
+//}
+//
+//@Composable
+//fun DemoKotlin() {
+//    val nama: String? = null
+//    val namaTampilan = nama ?: "Guest"
+//
+//    val nilaiAkhir = hitungNilaiAkhir(
+//        nilaiAwal = 75,
+//        bonus = 5
+//    )
+//
+//    val grade = tentukanGrade(nilaiAkhir)
+//    val status = tentukanStatus(nilaiAkhir)
+//
+//    Text(
+//        text = "Nama: $namaTampilan\n" +
+//                "Nilai akhir: $nilaiAkhir\n" +
+//                "Grade: $grade\n" +
+//                "Status: $status",
+//        modifier = Modifier.padding(32.dp)
+//    )
+//}
