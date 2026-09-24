26a46e7fb48bf0b48fa43360c66c06e80f41337e
shamhasan <ihtishamulhasaaan@gmail.com>
2026-09-24 18:38:56 +0700

    First commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.example.papbmodul"
-    compileSdk {
-        version = release(37)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.papbmodul"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -21,9 +20,11 @@ android {
 
     buildTypes {
         release {
-            optimization {
-                enable = false
-            }
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -32,6 +33,9 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
--- app/src/main/java/com/example/papbmodul/MainActivity.kt
+++ app/src/main/java/com/example/papbmodul/MainActivity.kt
@@ -1,112 +1,175 @@
 package com.example.papbmodul
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.papbmodul.ui.theme.PAPBModulTheme
 
 class MainActivity : ComponentActivity() {
+    val tag: String = "DEVELOPER TAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(tag, "Oncreate Terpanggil")
         enableEdgeToEdge()
         setContent {
+
             DemoKotlin()
         }
+
     }
 }
 
 @Composable
 fun DemoKotlin() {
-    Text(
-        text = "Belajar Dasar Kotlin",
-        modifier = Modifier.padding(32.dp)
+    val murid = DataStudent(
+        name = "Odellia",
+        score = 90
     )
+    val hasilObject = hasilStudentObject(
+        student = murid
+    )
+    val hasil = hasilStudent(
+        name = murid.name,
+        score = murid.score
+    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hasil Student Object",
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(
+            text = hasilObject,
+            modifier = Modifier.padding(8.dp)
+        )
+        Spacer(Modifier.height(16.dp))
+        Text(
+            text = "Hasil Student nama dan score",
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(
+            text = hasil,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
 }
 
-//@Composable
-//fun DemoKotlin() {
-//    Text(
-//        text = "Aplikasi Nilai Mahasiswa",
-//        modifier = Modifier.padding(32.dp)
-//    )
-//}
-
-//@Composable
-//fun DemoKotlin() {
-//    val nama = "Titin"
-//    val nilai = 85
-//
-//    Text(
-//        text = "Nama: $nama\nNilai: $nilai",
-//        modifier = Modifier.padding(32.dp)
-//    )
-//}
-
-//var dan val
-//val nilai = 85
-//nilai = 90
-//var nilai = 85
-//nilai = 90
-
-//null safety
-//@Composable
-//fun DemoKotlin() {
-//    val nama: String? = null
-//    val nilai = 85
-//
-//    val namaTampilan = nama ?: "Guest"
-//
-//    Text(
-//        text = "Nama: $namaTampilan\nNilai: $nilai",
-//        modifier = Modifier.padding(32.dp)
-//    )
-//}
-
 //bentuk kode keseluruhan
-//fun hitungNilaiAkhir(nilaiAwal: Int, bonus: Int = 0): Int {
-//    return nilaiAwal + bonus
-//}
-//
-//fun tentukanStatus(nilai: Int): String {
-//    return if (nilai >= 60) "Lulus" else "Tidak Lulus"
-//}
-//
-//fun tentukanGrade(nilai: Int): String {
-//    return when {
-//        nilai >= 80 -> "A"
-//        nilai >= 70 -> "B"
-//        nilai >= 60 -> "C"
-//        else -> "D"
-//    }
-//}
-//
-//@Composable
-//fun DemoKotlin() {
-//    val nama: String? = null
-//    val namaTampilan = nama ?: "Guest"
-//
-//    val nilaiAkhir = hitungNilaiAkhir(
-//        nilaiAwal = 75,
-//        bonus = 5
-//    )
-//
-//    val grade = tentukanGrade(nilaiAkhir)
-//    val status = tentukanStatus(nilaiAkhir)
-//
-//    Text(
-//        text = "Nama: $namaTampilan\n" +
-//                "Nilai akhir: $nilaiAkhir\n" +
-//                "Grade: $grade\n" +
-//                "Status: $status",
-//        modifier = Modifier.padding(32.dp)
-//    )
-//}
+fun hitungNilaiAkhir(nilaiAwal: Int, bonus: Int = 0): Int {
+    return nilaiAwal + bonus
+}
+
+fun tentukanStatus(nilai: Int): String {
+    return if (nilai >= 60) "Lulus" else "Tidak Lulus"
+}
+
+fun tentukanGrade(nilai: Int): String {
+    return when {
+        nilai >= 80 -> "A"
+        nilai >= 70 -> "B"
+        nilai >= 60 -> "C"
+        else -> "D"
+    }
+}
+
+// data, class, collection
+class auth() {
+    fun login() {
+        println("login cuy")
+    }
+
+    fun logout() {
+        println("sudah ter-logout")
+    }
+}
+
+
+class Student(
+    val name: String,
+    val score: Int,
+)
+
+data class DataStudent(val name: String, val score: Int)
+
+fun main() {
+    //data class
+    val data = DataStudent(
+        name = "Bigmo",
+        score = 75
+    )
+
+
+    val students = listOf<DataStudent>(
+        DataStudent("Espi", 90),
+        DataStudent("Adeyemi", 88),
+        DataStudent("Alvarez", 78),
+        DataStudent("Halland", 77),
+        DataStudent("Mbappe", 99),
+        DataStudent("Bellingham", 92),
+        DataStudent("Olise", 78),
+        DataStudent("Cunha", 70),
+    )
+
+    for (x in students) {
+        var msg: String;
+        val status: String = tentukanGrade(x.score)
+        if (status == "A") msg = "Good Job"
+        else msg = "Study More!!!"
+        println("${x.name} nilainya dapet ${x.score} nilai akhir ${status}, $msg")
+    }
+
+    hasilStudent(
+        name = data.name,
+        score = data.score,
+    )
+
+}
+
+fun getGrade(score: Int): String = when {
+    score >= 80 -> "A"
+    score >= 70 -> "B"
+    score >= 60 -> "C"
+    else -> "D"
+}
+
+fun getStatus(score: Int): String =
+    if (score >= 60) "Lulus" else "Tidak Lulus"
+
+fun hasilStudent(
+    name: String,
+    score: Int,
+): String {
+
+    val grade = getGrade(score)
+    val status = getStatus(score)
+
+    return "${name} | ${score} | $grade | $status |"
+}
+
+fun hasilStudentObject(
+    student: DataStudent,
+): String {
+
+    val grade = getGrade(student.score)
+    val status = getStatus(student.score)
+
+    return "${student.name} | ${student.score} | $grade | $status |"
+}
